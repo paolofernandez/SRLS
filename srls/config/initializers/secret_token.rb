30b7cd7e4a9fc172edc871fe000671140f3ee932
Paolo Adrian Fernandez Roman <piro_pafr@hotmail.com>
2013-09-08 21:02:34 -0400

    creando proyecto

---
+++ srls/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Srls::Application.config.secret_token = '7bbf977c7fdf9c50710fd44170759eca3844368c671da2fc851b1a17ba186a29aa836fd45ac401fa5175c6dd39b5d91abc3d71349b19ae58487959eaa8702468'
