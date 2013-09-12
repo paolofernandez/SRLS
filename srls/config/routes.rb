1e3db9bb395b5959ed343ad571c784ceb18f6d3f
Paolo Adrian Fernandez Roman <piro_pafr@hotmail.com>
2013-09-12 16:29:13 -0400

    con logs sin registrar

--- srls/config/routes.rb
+++ srls/config/routes.rb
@@ -1,6 +1,5 @@
 Srls::Application.routes.draw do
   
-  match 'user_managers' => 'user_managers#index'
   match 'user_managers/delete/:id' => 'user_managers#delete'
   match 'user_managers/new' => 'user_managers#new'
   match 'user_managers/create' => 'user_managers#create'
